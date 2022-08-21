ed06fc7faf8e4e072fa562d9dc844b4587c38de5
José <vitords1082@gmail.com>
2022-08-21 12:01:28 -0300

    .vscode/settings.json

---
+++ map.js
@@ -0,0 +1,36 @@
+// MAP Method
+const people = [
+    {
+        name: 'Klaus',
+        age: 345,
+        position: 'hybride',
+    },
+    {
+        name: 'Rebecca',
+        age: 156,
+        position: 'vampire',
+    },
+    {
+        name: 'Elijah',
+        age: 290,
+        position: 'vampire',
+    }
+];
+// Quokka.js - Exntension
+// returns a new array
+// does not change the size of original array(unlike filter)
+// uses values from original array when making new one
+
+const getAges = (person) => person.age * 2;
+
+const ages = people.map(getAges);
+console.log(ages);
+
+const newPeople = people.maps((item) => {
+    return {
+        firstName:item.name.toUpperCase(),
+        oldAge:item.age + 20
+    }
+})
+
+console.log(newPeople);
